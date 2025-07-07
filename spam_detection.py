ad7c6ebdb1f2735c966413659219165f072e5000
riyamahajan1006 <mahajanriya1006@gmail.com>
2025-07-07 11:07:59 +0530

    commiting

--- spam_detection.py
+++ spam_detection.py
@@ -3,7 +3,9 @@ import pandas as pd
 
 df = pd.read_csv('/content/spam.csv', encoding='ISO-8859-1')
 
-#Data preprocessing
+
+# ---------------------------------------  Data preprocessing -------------------------------------------------------
+
 df.info()
 # drop extra columns
 df.drop(columns=['Unnamed: 2',	'Unnamed: 3',	'Unnamed: 4'],inplace=True)
@@ -21,7 +23,9 @@ df.duplicated().sum()
 df.drop_duplicates(keep='first',inplace=True)
 df.duplicated().sum()
 
-#EDA
+
+#---------------------------------------------- EDA ----------------------------------------------------------
+
 df['target'].value_counts()
 #pie chart
 import matplotlib.pyplot as plt
@@ -52,3 +56,75 @@ sns.histplot(df[df['target']==0]['num_characters'],color='pink')
 sns.histplot(df[df['target']==1]['num_characters'])
 #relation btw columns
 sns.pairplot(df,hue='target')
+  
+
+# ------------------------------------------ text preprocessing -------------------------------------------------
+
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+stopwords.words('english')
+import string
+string.punctuation
+from nltk.stem.porter import PorterStemmer
+ps=PorterStemmer()
+#function
+def transform_text(text):
+  text=text.lower()
+  text=nltk.wordpunct_tokenize(text)
+  y=[]
+  for i in text:
+    if i.isalnum():
+      y.append(i)
+  text=y[:]
+  y.clear()
+  for i in text:
+    if i not in stopwords.words('english') and i not in string.punctuation:
+      y.append(i)
+  text=y[:]
+  y.clear()
+  for i in text:
+    y.append(ps.stem(i))
+  return y
+df['transformed_text']=df['text'].apply(transform_text)
+
+
+# -------------------------------  word cloud for spam --------------------------------------
+
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+# 1. Make sure text is string
+spam_wc = df[df['target']==1]['transformed_text'].apply(lambda x: " ".join(x) if isinstance(x, list) else str(x))
+
+# 2. Join all text
+text = spam_wc.str.cat(sep=" ")
+
+# 3. Generate WordCloud
+wc = WordCloud(width=500, height=500, min_font_size=10, background_color='white').generate(text)
+
+# 4. Plot
+plt.figure(figsize=(4,4))
+plt.imshow(wc, interpolation='bilinear')
+plt.axis('off')
+plt.show()
+
+
+# -------------------------------- word cloud for ham -----------------------------------------
+
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+# 1. Make sure text is string
+spam_wc = df[df['target']==0]['transformed_text'].apply(lambda x: " ".join(x) if isinstance(x, list) else str(x))
+
+# 2. Join all text
+text = spam_wc.str.cat(sep=" ")
+
+# 3. Generate WordCloud
+wc = WordCloud(width=500, height=500, min_font_size=10, background_color='white').generate(text)
+
+# 4. Plot
+plt.figure(figsize=(4,4))
+plt.imshow(wc, interpolation='bilinear')
+plt.axis('off')
+plt.show()
