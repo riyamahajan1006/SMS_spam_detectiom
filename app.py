6fcb0cea4c0e6550c235fca0681f521129e90bdb
riyamahajan1006 <mahajanriya1006@gmail.com>
2025-07-14 23:59:08 +0530

    commit

--- app.py
+++ app.py
@@ -5,36 +5,38 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
+# Download necessary resources if not already done
 nltk.download('punkt')
 nltk.download('stopwords')
-from nltk.tokenize import word_tokenize
-ps= PorterStemmer()
 
-tfidf=pickle.load(open('vectorizer.pkl', 'rb'))
-model=pickle.load(open('model.pkl', 'rb'))
+ps = PorterStemmer()
+stop_words = set(stopwords.words('english'))
+
+# Load vectorizer and model
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 def transform_text(text):
     text = text.lower()
     words = nltk.word_tokenize(text)
     words = [w for w in words if w.isalnum()]
-    words = [w for w in words if w not in stopwords.words('english')]
+    words = [w for w in words if w not in stop_words]
     words = [ps.stem(w) for w in words]
     return " ".join(words)
 
-
 st.title("SMS Spam Classifier")
 
-input_sms= st.text_input("Enter the SMS Message")
+input_sms = st.text_area("Enter the SMS Message")
 
-#preprocess
-transformed_text=transform_text(input_sms)
-#vectorise
-vector_input=tfidf.transform([transformed_text])
-#predict
-result=model.predict(vector_input)[0]
-#display
-if(result==1):
-    st.header("Spam")
-
-else:
-    st.header("Ham")
+if input_sms:
+    # Preprocess
+    transformed_text = transform_text(input_sms)
+    # Vectorize
+    vector_input = tfidf.transform([transformed_text])
+    # Predict
+    result = model.predict(vector_input)[0]
+    # Display
+    if result == 1:
+        st.header("Spam 🚫")
+    else:
+        st.header("Ham ✅")
